d2174ae7c4f47102a7897457753a46a37cbad745
asocjal <mailtocd@wp.pl>
2018-07-16 17:42:12 +0200

    Add logs

---
+++ src/main/java/lnzone/lib/utils/logs/Log.java
@@ -0,0 +1,37 @@
+package lnzone.lib.utils.logs;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import lnzone.lib.utils.FormattedTime;
+import lnzone.lib.utils.FormattedTime.Precision;
+import lnzone.lib.utils.Require;
+
+public class Log {
+	
+	FormattedTime time;
+	long timeMillsec;
+	private Map<String, Object> params = new HashMap<String, Object>();
+	@SuppressWarnings("unused")
+	private String event;
+	
+	public Log param(String name, Object value) {
+		params.put(name, value);
+		return this;
+	}
+	
+	public Log param(String name, long value) {
+		params.put(name, Long.toString(value));
+		return this;
+	}
+	
+	public void event(String message) {
+		Date now = new Date();
+		timeMillsec = now.getTime();
+		time = new FormattedTime(now, Precision.MILLISECONDS);
+		event = Require.notNull(message, "event");
+		Logs.getInstance().addLog(this);
+	}
+
+}

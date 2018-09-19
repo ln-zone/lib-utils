59faa7a817356ef765148626d790c88635ff9d3a
asocjal <mailtocd@wp.pl>
2018-09-19 17:58:04 +0200

    Add deepCopy method

--- src/main/java/lnzone/lib/utils/Utils.java
+++ src/main/java/lnzone/lib/utils/Utils.java
@@ -7,9 +7,15 @@ import com.google.gson.Gson;
 public class Utils {
 
 	public static boolean deepEquals(Object r1, Object r2) {
-		return StringUtils.equals((new Gson()).toJson(r1), (new Gson()).toJson(r2));
+		Gson gson = new Gson();
+		return StringUtils.equals(gson.toJson(r1), gson.toJson(r2));
 	}
-
+	
+	public static <T> T deepCopy(Object obj, Class<T> classOfT) {
+		Gson gson = new Gson();
+		return gson.fromJson(gson.toJson(obj), classOfT);
+	}	
+	
 	public static long rand() {
 		return (long)(Math.random()*Long.MAX_VALUE);
 	}

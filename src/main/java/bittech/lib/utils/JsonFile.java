bd88d11d0852d0d6d00bad7002868ab0e03e1d3d
asocjal <mailtocd@wp.pl>
2019-07-24 14:31:18 +0200

    Add JsonFile class

---
+++ src/main/java/bittech/lib/utils/JsonFile.java
@@ -0,0 +1,41 @@
+package bittech.lib.utils;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+
+import bittech.lib.utils.exceptions.StoredException;
+import bittech.lib.utils.json.JsonBuilder;
+
+public class JsonFile {
+
+	private transient String fileName = null;
+	
+	public JsonFile() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void changeFileName(String fileName) {
+		this.fileName = Require.notEmpty(fileName, "fileName");
+	}
+
+	public static <T extends JsonFile> T load(String fileName, Class<T> classOfT) {
+		try (FileReader fr = new FileReader(fileName)) {
+			T ret = JsonBuilder.build().fromJson(fr, classOfT);
+			ret.changeFileName(fileName);
+			return ret;
+		} catch (Exception ex) {
+			throw new StoredException("Cannot load settings from file " + fileName, ex);
+		}
+	}
+
+	public void save() {
+		if(fileName == null) {
+			// Just do not save
+		}
+		try (FileWriter fl = new FileWriter(fileName)) {
+			JsonBuilder.build().toJson(this, fl);
+		} catch (Exception ex) {
+			throw new StoredException("Cannot save settings to file " + fileName, ex);
+		}
+	}
+}

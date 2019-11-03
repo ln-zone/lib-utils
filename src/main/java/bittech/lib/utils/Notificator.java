5f0a90fb8ab7cf586995c89f0f4c7a8ff730a842
asocjal <mailtocd@wp.pl>
2019-11-03 16:25:36 +0100

    Better exception in notificator

--- src/main/java/bittech/lib/utils/Notificator.java
+++ src/main/java/bittech/lib/utils/Notificator.java
@@ -9,10 +9,14 @@ import java.util.concurrent.TimeUnit;
 import bittech.lib.utils.exceptions.StoredException;
 
 public class Notificator<T extends Object> implements AutoCloseable {
-
+	
 	private Set<T> observers = new HashSet<T>();
 
 	final ExecutorService singleThreadPool = Executors.newFixedThreadPool(1);
+	
+	public Notificator() {
+
+	}
 
 	public synchronized void register(T observer) {
 		observers.add(observer);
@@ -23,20 +27,31 @@ public class Notificator<T extends Object> implements AutoCloseable {
 	}
 
 	public synchronized void notifyThem(NotificationMethod<T> method) {
-
-		singleThreadPool.submit(() -> {
-			final ExecutorService threadPool = Executors.newFixedThreadPool(observers.size());
-			for (T el : observers) {
-				threadPool.submit(() -> method.notifyHim(el));
+		try {
+		
+			if (singleThreadPool.isShutdown()) {
+				throw new Exception("Thread pool was shutdown", null);
 			}
-			threadPool.shutdown();
-			try {
-				threadPool.awaitTermination(10, TimeUnit.SECONDS);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			
+			if (singleThreadPool.isTerminated()) {
+				throw new Exception("Thread pool was terminated", null);
 			}
-		});
+			
+			singleThreadPool.submit(() -> {
+				final ExecutorService threadPool = Executors.newFixedThreadPool(observers.size());
+				for (T el : observers) {
+					threadPool.submit(() -> method.notifyHim(el));
+				}
+				threadPool.shutdown();
+				try {
+					threadPool.awaitTermination(10, TimeUnit.SECONDS);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			});
+		} catch (Exception ex) {
+			throw new StoredException("Cannot notify listeners", ex);
+		}
 	}
 
 	@Override

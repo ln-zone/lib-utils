cda888c4729c074b067d9e2052f05c25533f6f15
asocjal <mailtocd@wp.pl>
2019-11-14 23:28:11 +0100

    Add ExceptionsToLogs converter

--- src/main/java/bittech/lib/utils/exceptions/ExceptionManager.java
+++ src/main/java/bittech/lib/utils/exceptions/ExceptionManager.java
@@ -7,6 +7,8 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import bittech.lib.utils.Config;
+
 public class ExceptionManager {
 
 	private final Logger LOGGER = LoggerFactory.getLogger(ExceptionManager.class);
@@ -14,9 +16,19 @@ public class ExceptionManager {
 	private static final ExceptionManager instance = new ExceptionManager();
 
 	private Map<Long, ExceptionInfo> exceptions = new ConcurrentHashMap<Long, ExceptionInfo>();
+	
+	private final static boolean pushToLogs = Config.getInstance().getEntryOrDefault("pushToLogs", Boolean.class, true);
+	
+	private ExceptionsToLogsConverters exceptionsToLogsConverters = new ExceptionsToLogsConverters();
 
 	private ExceptionManager() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public synchronized void reset() {
+		exceptions.clear();
+		exceptionsToLogsConverters.close();
+		exceptionsToLogsConverters = new ExceptionsToLogsConverters();
 	}
 
 	public static ExceptionManager getInstance() {
@@ -26,27 +38,42 @@ public class ExceptionManager {
 	public synchronized void deleteAll() {
 		exceptions.clear();
 	}
-	
+
 	public Collection<Long> getExceptionIds() {
 		return exceptions.keySet();
 	}
-	
+
+	public synchronized boolean contains(long exceptionId) {
+		return exceptions.containsKey(exceptionId);
+	}
+
 	public synchronized long add(StoredException exception) {
 		long exceptionId;
 		if (exception.getCause() instanceof StoredException) {
 			exceptionId = ((StoredException) exception.getCause()).getId();
-		} else {
-			do {
-				exceptionId = (long) (Math.random() * Long.MAX_VALUE);
-			} while (exceptions.containsKey(exceptionId));
+			exceptions.remove(exceptionId);
 		}
+
+		do {
+			exceptionId = (long) (Math.random() * Long.MAX_VALUE);
+		} while (exceptions.containsKey(exceptionId));
+
 		exceptions.put(exceptionId, new ExceptionInfo(exception));
 		LOGGER.debug("Exception with id " + exceptionId + " created");
+		
+		if(pushToLogs) {
+			exceptionsToLogsConverters.push(exception);
+		}
+		
 		return exceptionId;
 	}
 
 	public synchronized ExceptionInfo get(long exceptionId) {
 		return exceptions.get(exceptionId);
+	}
+	
+	public void registerConverter(ExceptionToLogConverter converter) {
+		exceptionsToLogsConverters.registerConverter(converter);
 	}
 
 //	public String getAsString(long exceptionId) {
--- src/main/java/bittech/lib/utils/exceptions/StoredException.java
+++ src/main/java/bittech/lib/utils/exceptions/StoredException.java
@@ -1,5 +1,8 @@
 package bittech.lib.utils.exceptions;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,19 +18,37 @@ public class StoredException extends RuntimeException {
 	
 	private final long id;
 	
+	private final long timestamp;
+	
 	private final static boolean print = Config.getInstance().getEntryOrDefault("printExceptions", Boolean.class, true);
-
+	
 	public StoredException(String message, Throwable cause) {
 		super(message, cause);
 		id = exceptionManager.add(this);
+		timestamp = System.currentTimeMillis();
 		LOGGER.debug("Exception created with id " + id +" : " + message);
 		if(print) {
 			this.printStackTrace();
 		}
+
 	}
 	
 	public long getId() {
 		return id;
+	}
+	
+	public long getTimestamp() {
+		return timestamp;
+	}
+	
+	public List<String> listReasons() {
+		List<String> reasons = new LinkedList<String>();
+		Throwable myEx = this.getCause();
+		while(myEx != null) {
+			reasons.add(myEx.getMessage());
+			myEx = myEx.getCause();
+		}
+		return reasons;
 	}
 
 

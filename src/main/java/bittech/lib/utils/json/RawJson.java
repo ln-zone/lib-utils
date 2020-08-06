5b07957572007be0df7942448f8c30e2e7ca3b34
asocjal <mailtocd@wp.pl>
2020-08-06 11:32:24 +0200

    WiP

--- src/main/java/bittech/lib/utils/json/RawJson.java
+++ src/main/java/bittech/lib/utils/json/RawJson.java
@@ -1,6 +1,9 @@
 package bittech.lib.utils.json;
 
 import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+
+import bittech.lib.utils.exceptions.StoredException;
 
 public class RawJson implements Serializable {
 
@@ -22,6 +25,14 @@ public class RawJson implements Serializable {
 
 	public String toString() {
 		return str;
+	}
+
+	public byte[] getBytes() {
+		try {
+			return str.getBytes("UTF8");
+		} catch (UnsupportedEncodingException e) {
+			throw new StoredException("Failed to get bytes of Json", e);
+		}
 	}
 
 	public <T> T fromJson(Class<T> classOfT) {
